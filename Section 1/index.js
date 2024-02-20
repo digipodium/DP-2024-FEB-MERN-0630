c797f67266194b941613414a06122dd77978fa1c
m-triple-m <triplem656@gmail.com>
2024-02-20 09:42:41 +0530

    arrays

---
+++ Section 1/index.js
@@ -0,0 +1,38 @@
+console.log('hello javascript');
+
+console.log('good');
+
+num = 724;
+console.log(num);
+
+myname = "Mubassir";
+
+console.log(myname);
+console.log( typeof myname );
+
+ishungry = true;
+console.log(ishungry);
+
+// exponential operator
+console.log(2**3);
+console.log(64 ** 0.5 );
+
+console.log( 1 + '1' );
+console.log( true + 'something' );
+console.log( 5 + 1 + '4' );
+
+console.log( 1 == '1' );
+console.log( 1 === '1' );
+
+if(45%2 === 0){
+    console.log('even');
+}else{
+    console.log('odd');
+}
+
+age = 35;
+if(age >=18){
+    console.log('you are eligible for DL');
+}else{
+    console.log('you are not eligible for DL');
+}
