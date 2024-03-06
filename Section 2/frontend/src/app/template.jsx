c5898ea28090be2b208875015a814ca8e97da479
m-triple-m <triplem656@gmail.com>
2024-03-06 20:01:18 +0530

    form handling

---
+++ Section 2/frontend/src/app/template.jsx
@@ -0,0 +1,21 @@
+'use client';
+import Navbar from '@/components/Navbar';
+import React, { useEffect } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Template = ({children}) => {
+
+    useEffect(() => {
+      require('bootstrap/dist/js/bootstrap.min.js');
+    }, [])
+    
+
+  return (
+    <div>
+        <Navbar />
+        {children}
+    </div>
+  )
+}
+
+export default Template;
